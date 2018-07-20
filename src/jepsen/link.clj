17f692720393960f73190d16ec2527a3979067cc
Xavier Riley <xavriley@hotmail.com>
2018-07-20 22:43:46 +0100

    Add tests for reads
    Treat tempo as a single register. Read tests implemented, writes to
    follow. Took a while to work out that the process needed to be in the
    background. Also had to adapt the test script to run a TCP server to
    allow the control node to have access to specific sessions.

--- src/jepsen/link.clj
+++ src/jepsen/link.clj
@@ -2,8 +2,10 @@
   (:require [clojure.tools.logging :refer :all]
             [clojure.string :as str]
             [jepsen [cli :as cli]
+                    [client :as client]
                     [control :as c]
                     [db :as db]
+                    [generator :as gen]
                     [tests :as tests]]
             [jepsen.control.util :as cu]
             [jepsen.os.debian :as debian]))
@@ -22,6 +24,7 @@
                          :cmake
                          :git
                          :libssl-dev
+                         :netcat
                          :ruby-dev
                          :rubygems
                          :libglib2.0-dev])
@@ -39,16 +42,42 @@
                     (c/exec :bundle :install)
                     (c/exec :rake :compile)
                     (info node "Link installed - starting test")
-                    (c/exec (c/lit "ruby -Ilib -e 'require \"bundler/setup\"; require \"ableton_link\"; link = AbletonLink.new; link.enable; 100.times { puts link.status; sleep link.time_until_subdivision_within_beat(0.0).abs }' > /link.log 2>&1 "))))))
+                    (c/exec (c/lit "bin/server > /link.log 2>&1 &"))))))
 
     (teardown! [_ test node]
       (c/su
-        (c/exec :rm :-rf "/ruby_ableton_link"))
+        ;; exit gracefully from the process to avoid failing the test
+        (c/exec (c/lit "echo 'quit' | nc localhost 17001"))
+        (c/exec (c/lit "rm -rf /ruby_ableton_link")))
       (info node "tearing down link"))
 
     db/LogFiles
     (log-files [_ test node]
       [logfile])))
+
+(defn r   [_ _] {:type :invoke, :f :read, :value nil})
+(defn w   [_ _] {:type :invoke, :f :write, :value (+ 20 (rand-int 200))})
+
+;; this may not be relevant
+;; (defn cas [_ _] {:type :invoke, :f :cas, :value [(rand-int 5) (rand-int 5)]})
+
+(defrecord Client [conn]
+  client/Client
+  (open! [this test node]
+    (assoc this :node node))
+
+  (setup! [this test])
+
+  (invoke! [this test op]
+    (case (:f op)
+        :read (assoc op :type :ok,
+                     ;; unsafe
+                     :value (read-string (c/exec (c/lit (str "echo 'status' | nc " (:node this) " 17001")))))
+        ))
+
+  (teardown! [this test])
+
+  (close! [_ test]))
 
 (defn link-test
   "Given an options map from the command line runner (e.g. :nodes, :ssh,
@@ -58,7 +87,12 @@
          opts
          {:name "link"
           :os debian/os
-          :db (db "0.0.1")}))
+          :db (db "0.0.1")
+          :client (Client. nil)
+          :generator (->> r
+                          (gen/stagger 1)
+                          (gen/nemesis nil)
+                          (gen/time-limit 15))}))
 
 (defn -main
   "Handles command line arguments. Can either run a test, or a web server for
