072daf7d9ad9b4a06d4f523349ad9ef5706a8bda
Xavier Riley <xavriley@hotmail.com>
2018-07-27 23:33:53 +0100

    Tidy up project.clj

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
 (defproject jepsen.link "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Jepsen tests for the Ableton Link music timing protocol"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :main jepsen.link
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [jepsen "0.1.8"]
-                 [verschlimmbesserung "0.1.3"]])
+                 [jepsen "0.1.8"]])
